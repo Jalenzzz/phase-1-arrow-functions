8b6d4f9666c3168a24688fb4ef3f2e6db004db37
Jalen Mnene <mnenejalen72@gmail.com>
2024-04-03 12:06:04 +0300

    Finished arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+// Arrow function to perform division
+const divide = (dividend = 2000, divisor = 100) => dividend / divisor;
+const result = divide();
+
+// Arrow function for squaring a number
+const square = (number) => number * number;
+
+// Arrow function for adding two numbers
+const add = (num1, num2) => num1 + num2;
